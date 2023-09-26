f8b19bd43a506006b55c95d27151d9e42cbce83e
Diana Medina <dianaalauramn@gmail.com>
2023-09-26 13:33:18 -0700

    Card Song component was updated to handle the tracks to guess playlist

--- src/components/cardSongComponent/CardSongComponent.tsx
+++ src/components/cardSongComponent/CardSongComponent.tsx
@@ -1,24 +1,48 @@
 import style from "./CardSongComponent.module.css";
-import album from "./../../img/album.svg";
+import Track from "../../entities/track/Track";
+import React, { useEffect } from "react";
+import usePlay from "../../hooks/usePlay";
+import useGameConfig from "../../hooks/useGameConfig";
+import RenderGameGuessTrackComponent from "../renderGameGuessTrack/RenderGameGuessTrackComponent";
 
-const CardSongComponent = () => {
+interface CardSongComponentProps {
+    tracks: Track[];
+}
+
+const CardSongComponent: React.FC<CardSongComponentProps> = ({ tracks }) => {
+
+    const { playState: { currentTrackIndex }, handleOnChangeCurrentTrack } = usePlay();
+    const { configurationGame: { timerListen, timerSong, timerGuess }, handleOnActiveSong, handleOnActiveListen } = useGameConfig();
+    const currentTrack = tracks[currentTrackIndex];
+
+    const timerToLoadNextTrack = () => {
+        return ((timerListen.time * 1000) + (timerGuess.time * 1000) + (timerSong.time * 1000))
+    }
+
+    useEffect(() => {
+        setTimeout(() => {
+            handleOnActiveSong(false);
+            handleOnChangeCurrentTrack(currentTrackIndex < tracks.length - 1 ? currentTrackIndex + 1 : 0);
+            handleOnActiveListen(true);
+        }, timerToLoadNextTrack());
+    }, [currentTrack]);
+
     return (
         <div className={style.cardSongContainer}>
             <div className={style.titleContainer}>
                 <h1>Guess The Song</h1>
             </div>
-            <div className={style.albumContainer}>
-                <div className={style.albumCover}>
-                    <img src={album} alt="Album" />
-                </div>
-                <div className={style.albumDataContainer}>
-                    <h2 className={style.titleSong}>0X1=LOVESONG (I Know I Love You) feat.Seori</h2>
-                    <h3 className={style.artistSong}>TOMORROW X TOGETHER</h3>
-                    <h3 className={style.albumSong}>THE CHAOS CHAPTER: FIGHT OR ESCAPE</h3>
-                </div>
+            <div className={style.trackContainer}>
+                {currentTrack && (
+                    <div>
+                        <RenderGameGuessTrackComponent
+                            track={currentTrack}
+                        />
+                    </div>
+                )}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default CardSongComponent
+export default CardSongComponent;
