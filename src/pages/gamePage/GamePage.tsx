27d8f6f1e6d27997ba0b9be773fd03adca205f23
Diana Medina <dianaalauramn@gmail.com>
2023-09-26 13:19:46 -0700

    Game Page was updated to implement Game and Play context

--- src/pages/gamePage/GamePage.tsx
+++ src/pages/gamePage/GamePage.tsx
@@ -1,22 +1,31 @@
+import style from "./GamePage.module.css"
 import CardSelectSongComponent from "../../components/CardSelectSongComponent/CardSelectSongComponent"
 import CardSongComponent from "../../components/cardSongComponent/CardSongComponent"
-import style from "./GamePage.module.css"
+import useGameConfig from "../../hooks/useGameConfig"
+import usePlay from "../../hooks/usePlay"
+
 const GamePage = () => {
+    const { playState } = usePlay();
+    const { score } = playState;
+    const { configurationGame } = useGameConfig();
+    const { tracks, level } = configurationGame;
+    if (!tracks) { throw new Error('Tracks in game page') }
+
     return (
         <div className={style.gamePageContainer}>
             <div className={style.levelScoreContainer}>
                 <div className={style.levelContainer}>
                     <h2>Level</h2>
-                    <h3>Easy</h3>
+                    <h3>{level}</h3>
                 </div>
                 <div className={style.scoreContainer}>
                     <h2 >Score</h2>
-                    <h3 >199</h3>
+                    <h3 >{score}</h3>
                 </div>
             </div>
             <div className={style.cardContainer}>
                 <div className={style.card1}>
-                    <CardSongComponent />
+                    <CardSongComponent tracks={tracks} />
                 </div>
                 <div className={style.card2}>
                     <CardSelectSongComponent />
