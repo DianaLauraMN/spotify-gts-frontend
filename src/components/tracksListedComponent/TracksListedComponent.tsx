20d2877c72b0bd30d95bc5c0fafe0ec7de227229
Diana Medina <dianaalauramn@gmail.com>
2023-10-02 12:47:44 -0700

    RecentlyPlayedComponent was renamed to TracksListedComponent due to the change in its functionality

--- src/components/tracksListedComponent/TracksListedComponent.tsx
+++ src/components/tracksListedComponent/TracksListedComponent.tsx
@@ -2,15 +2,33 @@ import style from "./RecentlyPlayedComponent.module.css";
 import React, { useEffect, useState } from "react";
 import useGTS from "../../hooks/useGTS";
 import Track from "../../entities/track/Track";
+import usePlay from "../../hooks/usePlay";
 
 const RecentlyPlayedComponent = () => {
-    const { gtsState: { tracksRecentlyPlayed, searchResultsTracks }, loadTracksRecentlyPlayed } = useGTS();
     const [tracks, setTracks] = useState<Track[]>([]);
-    loadTracksRecentlyPlayed();
+    const [trackCardEnabled, setTrackCardEnabled] = useState(true);
+    const { gtsState: { tracksRecentlyPlayed, searchResultsTracks }, loadTracksRecentlyPlayed } = useGTS();
+    const { playState: { trackAnswer, currentTrack }, handleOnChangeAsserts, handleOnChangeFailed, handleOnChangeTrackAnswer } = usePlay();
+
+    if (tracksRecentlyPlayed.length === 0) { loadTracksRecentlyPlayed(); }
 
     useEffect(() => {
         searchResultsTracks.length > 0 ? setTracks(searchResultsTracks) : setTracks(tracksRecentlyPlayed);
-    }, [searchResultsTracks,tracksRecentlyPlayed]);
+    }, [searchResultsTracks, tracksRecentlyPlayed]);
+
+    useEffect(() => {
+        setTrackCardEnabled(true);
+    }, [currentTrack]);
+
+    useEffect(() => {
+        assignsGameResults();
+    }, [trackAnswer]);
+
+    const assignsGameResults = () => {
+        if (trackAnswer && currentTrack) {
+            ((trackAnswer.id === currentTrack.id) || (trackAnswer.name === currentTrack.name)) ? handleOnChangeAsserts(currentTrack) : handleOnChangeFailed(currentTrack);
+        }
+    }
 
     return (
         <div className={style.recentlyPlayedContainer}>
@@ -21,7 +39,12 @@ const RecentlyPlayedComponent = () => {
                             tracks.map((track, key) => (
                                 <div key={key}>
 
-                                    <div className={style.cardSong}>
+                                    <div id="cardSong" className={style.cardSong} onClick={() => {
+                                        if (trackCardEnabled) {
+                                            handleOnChangeTrackAnswer(track);
+                                            setTrackCardEnabled(false);
+                                        }
+                                    }}>
                                         <div className={style.coverAlbum}>
                                             <img src={track.album.images[0].url} alt="" />
                                         </div>
@@ -29,7 +52,7 @@ const RecentlyPlayedComponent = () => {
                                             <h3>{track.name}</h3>
                                             <h4>
                                                 {track.artists.map((artist) => (
-                                                    <React.Fragment key={artist.id}>{artist.name}</React.Fragment>
+                                                    <React.Fragment key={artist.id}>{artist.name + ' '}</React.Fragment>
                                                 ))}
                                             </h4>
                                         </div>
