57b291be1a8b544629bbc9e475d111b9dc384f03
Diana Medina <dianaalauramn@gmail.com>
2023-10-16 12:53:48 -0700

    CircularProgressWithLabel component changes in comments due to refactor

--- src/components/CircularProgressWithLabel/CircularProgressWithLabel.tsx
+++ src/components/CircularProgressWithLabel/CircularProgressWithLabel.tsx
@@ -3,28 +3,49 @@ import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
 import useGame from '../../hooks/useGame';
 import WavesAudioComponent from '../wavesAudio/WavesAudioComponent';
+import usePlay from '../../hooks/usePlay';
 
 const CircularProgressWithLabel: React.FC = () => {
+  const { playState: { currentTrack, trackAnswer, timerUser }, handleOnChangeTimerUser } = usePlay();
   const { configurationGame: { durationMs, timerListen }, handleOnActiveGuess, handleOnActiveListen, handleOnActiveSong } = useGame();
   const value = durationMs;
   const [progress, setProgress] = useState<number>(0);
 
   useEffect(() => {
+    let startTime: number; // Almacena el tiempo de inicio del timer
+
+    // if (trackAnswer) {
+    //   handleOnActiveListen(false);
+    //   handleOnActiveGuess(false);
+    //   handleOnActiveSong(true);
+    // } else 
     if (timerListen.active) {
+
+      startTime = Date.now(); // Guarda el tiempo de inicio cuando comienza el timer
+
       const timer = setInterval(() => {
         setProgress((prevProgress) => (prevProgress >= value ? durationMs : prevProgress + 1));
+
+        // Calcula la duración del timer en segundos
+        const endTime = Date.now();
+        const elapsedSeconds = Math.floor((endTime - startTime) / 1000);
+        handleOnChangeTimerUser(elapsedSeconds);
+        console.log(`El timer se ejecutó durante ${elapsedSeconds} segundos.`);
       }, 1000);
 
+      //const timerId = 
       setTimeout(() => {
         handleOnActiveListen(false);
         handleOnActiveSong(false);
         handleOnActiveGuess(true);
-      }, timerListen.time * 1000);
+        //console.log('realmente el timer se detiene aqui');
+      }, timerUser * 1000);
       return () => {
         clearInterval(timer);
+        //  clearTimeout(timerId);
       };
     }
-  }, []);
+  }, []); //currentTrack, trackAnswer
 
   const circularProgressStyle = {
     color: '#181818'
@@ -51,7 +72,7 @@ const CircularProgressWithLabel: React.FC = () => {
             justifyContent: 'center',
           }}
         >
-          <WavesAudioComponent />
+                    <WavesAudioComponent />
         </Box>
       </Box>
     </div>
