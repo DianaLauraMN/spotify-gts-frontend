496bb8c401b23338c9e05f76d5e11702d51731b1
Diana Medina <dianaalauramn@gmail.com>
2023-11-08 17:15:46 -0700

    Spotify button was updated due to refactorization

--- src/components/utilitiesComponents/spotifyButton/SpotifyButton.tsx
+++ src/components/utilitiesComponents/spotifyButton/SpotifyButton.tsx
@@ -1,11 +1,10 @@
 import style from "./SpotifyButton.module.css"
-import { json, useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import Global from "../../../Global/Global";
-import ApiAuth from "../../../api/ApiAuth";
 import useGame from "../../../hooks/useGame";
+import useAuth from "../../../hooks/useAuth";
 
-const apiAuth = new ApiAuth();
 const spoty_url = `https://accounts.spotify.com/authorize?client_id=${Global.client_id}&response_type=code&redirect_uri=${Global.redirect_uri}&scope=${Global.scopes}`;
 
 interface SpotifyButtonProps {
@@ -13,23 +12,15 @@ interface SpotifyButtonProps {
 }
 
 const SpotifyButton: React.FC<SpotifyButtonProps> = ({ type }) => {
+
     const { handleOnSubmitConfigGame, configurationGame } = useGame();
+    const { isLoggedIn, apiAuth } = useAuth();
 
     const navigate = useNavigate();
     const location = useLocation();
 
-    const handleLoginClick = () => {
-        const authenticated: boolean = isAuthenticated();
-        if (authenticated) {
-            // console.log(JSON.parse(localStorage.getItem("access_token")));
-
-            // console.log(localStorage.access_token);
-            // console.log(localStorage.refresh_token);
-
-            login(); //no hace que se autentique de nuevo pero refresca el token al parecer
-        } else {
-            login();
-        }
+    const handleLoginClick = async () => {
+        isLoggedIn ? navigate('/configGame') : window.location.replace(spoty_url);
     };
 
     const handleGameClick = () => {
@@ -37,38 +28,24 @@ const SpotifyButton: React.FC<SpotifyButtonProps> = ({ type }) => {
     };
 
     useEffect(() => {
-        if (type === 'login') {
-            const timer = setTimeout(() => {
+        const authenticateUser = async () => {
+            if (type === 'login') {
                 const urlParams = new URLSearchParams(location.search);
-                const spotyCode = urlParams.get("code");
-                if (spotyCode) authenticateUser(spotyCode);
-            }, 500); // 0.5 segundos
-            return () => clearTimeout(timer);// Limpieza del useEffect si es necesario (cancelar el timer)
-        }
-    }, [])
+                const spotifyCode = urlParams.get('code');
+                if (spotifyCode) {
+                    await apiAuth.getCredentials(spotifyCode);
+                    navigate('/configGame');
+                }
+            }
+        };
 
-    const authenticateUser = async (spotyCode: string) => {
-        try {
-            const accessToken = await apiAuth.getAccessToken(spotyCode);
-            if (accessToken) navigate("/configGame");
-        } catch (error) {
-            console.log('Error while authentication:', error);
-        }
-    };
-
-    const isAuthenticated = () => {
-        return localStorage.access_token ? true : false;
-    }
+        authenticateUser();
+    }, [location.search]);
 
     const startGame = async () => {
         await handleOnSubmitConfigGame(configurationGame);
         navigate("/game")
     }
-
-
-    function login() {
-        window.location.replace(spoty_url);
-    };
 
     return (
         <button className={style.btnLogin} onClick={type === 'login' ? handleLoginClick : handleGameClick}>
@@ -76,5 +53,7 @@ const SpotifyButton: React.FC<SpotifyButtonProps> = ({ type }) => {
         </button>
     );
 }
+
+
 
 export default SpotifyButton;
