0e8adf46f09d9a427795f80cf5250bac3c848cc1
Diana Medina <dianaalauramn@gmail.com>
2023-12-04 12:21:16 -0700

    Minor changes on files

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -25,32 +25,3 @@ const useAuth = () => {
 };
 
 export default useAuth;
-
-// import { useEffect, useState, useMemo } from 'react';
-// import ApiAuth from '../api/ApiAuth';
-// import AuthDate from '../api/AuthDate';
-
-// const useAuth = () => {
-//     const [isLoggedIn, setIsLoggedIn] = useState(false);
-//     const apiAuth = useMemo(() => new ApiAuth(), []); // Crea la instancia solo una vez
-//     const authDate = new AuthDate();
-
-//     useEffect(() => {
-//         const checkAuthentication = async () => {
-//             if (authDate.isAuthDateValid()) {
-//                 if (apiAuth.isTokenValid()) {
-//                     setIsLoggedIn(true);
-//                 } else if (apiAuth.doesRefreshTokenExist()) {
-//                     await apiAuth.getRefreshedToken();
-//                     setIsLoggedIn(true);
-//                 }
-//             }
-//         };
-//         checkAuthentication();
-//     }, [apiAuth, authDate]);
-
-//     return { isLoggedIn, apiAuth };
-// };
-
-// export default useAuth;
-
