61674a17d2655b0981a617061ce31efde4bc023f
Diana Medina <dianaalauramn@gmail.com>
2024-01-22 18:28:06 -0700

    Game page updated

--- src/pages/gamePage/GamePage.tsx
+++ src/pages/gamePage/GamePage.tsx
@@ -4,47 +4,50 @@ import CardSelectSongComponent from "../../components/CardSelectSongComponent/Ca
 import CardSongComponent from "../../components/cardSongComponent/CardSongComponent";
 import useGame from "../../hooks/useGame";
 import usePlay from "../../hooks/usePlay";
-import SpotifyButton from '../../components/utilitiesComponents/spotifyButton/SpotifyButton';
-import { useNavigate } from 'react-router-dom';
+import CircleLoadingComponent from '../../components/utilitiesComponents/circleLoadingComponent/CircleLoadingComponent';
 
 const GamePage = () => {
-    const { playState: { score, failed, asserts }, handleOnChangeScore } = usePlay();
-    const { configurationGame: { level, tracksQuantity } } = useGame();
-
-    const navigate = useNavigate();
+    const { playState: { score, asserts }, handleOnChangeScore } = usePlay();
+    const { configurationGame: { level, tracksQuantity, tracks, areTracksLoaded }, handleAreTracksLoaded } = useGame();
 
     useEffect(() => {
-        // handleOnChangeScore((asserts.length / tracksQuantity) * 100);
         const score = (asserts.length / tracksQuantity) * 100;
         const scoreFixed = score.toFixed(2);
         const castedScore = parseFloat(scoreFixed);
         handleOnChangeScore(castedScore);
-    }, [asserts]);
+        tracks && tracks?.length > 0 ? handleAreTracksLoaded(true) : handleAreTracksLoaded(false);
+    }, [asserts, tracks, areTracksLoaded]);
 
     return (
-        <div className={style.gamePageContainer}>
-            <div className={style.levelScoreContainer}>
-                <div className={style.levelContainer}>
-                    <h2>Level</h2>
-                    <h3>{level}</h3>
+        <div>
+            {!areTracksLoaded && (
+                <CircleLoadingComponent />
+            )}
+
+            {areTracksLoaded && (
+                <div className={style.gamePageContainer}>
+                    <div className={style.levelScoreContainer}>
+                        <div className={style.levelContainer}>
+                            <h2>Level</h2>
+                            <h3>{level === 0 && 'Easy'}</h3>
+                            <h3>{level === 1 && 'Normal'}</h3>
+                            <h3>{level === 2 && 'Hard'}</h3>
+                        </div>
+                        <div className={style.scoreContainer}>
+                            <h2 >Score</h2>
+                            <h3 >{score} / 100</h3>
+                        </div>
+                    </div>
+                    <div className={style.cardContainer}>
+                        <div className={style.card1}>
+                            <CardSongComponent />
+                        </div>
+                        <div className={style.card2}>
+                            <CardSelectSongComponent />
+                        </div>
+                    </div>
                 </div>
-                <div className={style.scoreContainer}>
-                    <h2 >Score</h2>
-                    <h3 >{score} / 100</h3>
-                    <h2 >Asserts</h2>
-                    <h3 >{asserts.length}</h3>
-                    <h2 >Failed</h2>
-                    <h3 >{failed.length}</h3>
-                </div>
-            </div>
-            <div className={style.cardContainer}>
-                <div className={style.card1}>
-                    <CardSongComponent />
-                </div>
-                <div className={style.card2}>
-                    <CardSelectSongComponent />
-                </div>
-            </div>
+            )}
         </div>
     )
 }
