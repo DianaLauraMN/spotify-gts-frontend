97bb9406b938fd8dafaf39cf9b364b676f21fc0f
Diana Medina <dianaalauramn@gmail.com>
2024-02-15 20:34:42 -0700

    validation of Steps added on CircularProgressComponent

--- src/components/circularProgressComponent/CircularProgressComponent.tsx
+++ src/components/circularProgressComponent/CircularProgressComponent.tsx
@@ -31,31 +31,36 @@ const CircularProgressWithLabel: React.FC = () => {
     color: '#181818'
   };
   return (
-    <div>
-      <Box sx={{ position: 'relative', display: 'inline-flex' }}>
-        <CircularProgress
-          variant="determinate"
-          value={(progress / durationMs) * 100}
-          size={300}
-          thickness={1.2}
-          sx={circularProgressStyle}
-        />
-        <Box
-          sx={{
-            top: 0,
-            left: 0,
-            bottom: 0,
-            right: 0,
-            position: 'absolute',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <WavesAudioComponent />
-        </Box>
-      </Box>
-    </div>
+    <>
+      {(gameStep === Steps.LISTEN || gameStep === Steps.GUESS) &&
+        <div>
+          <Box sx={{ position: 'relative', display: 'inline-flex' }}>
+            <CircularProgress
+              variant="determinate"
+              value={(progress / durationMs) * 100}
+              size={300}
+              thickness={1.2}
+              sx={circularProgressStyle}
+            />
+            <Box
+              sx={{
+                top: 0,
+                left: 0,
+                bottom: 0,
+                right: 0,
+                position: 'absolute',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              <WavesAudioComponent />
+            </Box>
+          </Box>
+        </div>
+      }
+    </>
+
   );
 };
 
